d723c1c96fa9c7cc674dbdcbb944cd1555d6998a
Bair <aiusheev@gmail.com>
2023-09-25 20:32:19 -0400

    Replace @EqualsAndHashCode

--- src/main/java/com/baiu/hrrch/groupcatalog/GroupCatalog.java
+++ src/main/java/com/baiu/hrrch/groupcatalog/GroupCatalog.java
@@ -1,18 +1,17 @@
 package com.baiu.hrrch.groupcatalog;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.EqualsAndHashCode;
 import com.baiu.hrrch.catalog.Catalog;
 import com.baiu.hrrch.group.Group;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import org.hibernate.proxy.HibernateProxy;
 
 @Entity(name = "group_catalog")
-@EqualsAndHashCode
 public class GroupCatalog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -68,5 +67,27 @@ public class GroupCatalog {
 
     public void setCanEdit(boolean canEdit) {
         this.canEdit = canEdit;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        GroupCatalog that = (GroupCatalog) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
--- src/main/java/com/baiu/hrrch/person/Person.java
+++ src/main/java/com/baiu/hrrch/person/Person.java
@@ -1,10 +1,10 @@
 package com.baiu.hrrch.person;
 
+import com.baiu.hrrch.group.Group;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.EqualsAndHashCode;
-import com.baiu.hrrch.group.Group;
-
+import java.util.Objects;
+import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -13,11 +13,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import java.util.Set;
+import org.hibernate.proxy.HibernateProxy;
 
 
 @Entity(name = "person")
-@EqualsAndHashCode
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -98,5 +97,27 @@ public class Person {
     @Override
     public String toString() {
         return login;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        Person person = (Person) o;
+        return getId() != null && Objects.equals(getId(), person.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
